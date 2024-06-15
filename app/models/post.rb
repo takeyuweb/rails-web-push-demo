fade072e103686e0f284aacc75d6d13bce812b64
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2024-06-15 11:30:11 +0000

    template_id の指定はやめる

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,14 @@ class Post < ApplicationRecord
     api_instance = OneSignal::DefaultApi.new
     notification = OneSignal::Notification.new(
       app_id: Rails.application.credentials.dig(:onesignal, :app_id),
-      included_segments: ["Total Subscriptions", "Inactive Subscriptions", "Active Subscriptions"],
+      included_segments: ["Total Subscriptions"],
       # https://documentation.onesignal.com/reference/push-channel-properties
+      # https://github.com/OneSignal/onesignal-ruby-api/blob/master/docs//Notification.md
       contents: {
         en: body,
+        es: body,
       },
-      template_id: Rails.application.credentials.dig(:onesignal, :template_id),
+      is_any_web: true, # 指定がないとどこにも届かない #<OneSignal::InlineResponse200:0x00007f86b02e6e70 @id="", @errors=["All included players are not subscribed"]>
     )
 
     Rails.logger.info api_instance.create_notification(notification).inspect
